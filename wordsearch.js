f845d09bed44ca6a8a91bff72cc21609febbe45b
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-17 01:59:21 -0400

    Implemented program for vertical and horizontal wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(horizontalArr => horizontalArr.join(''));
+  const verticalJoin = transpose(letters).map(x => x.join(''));
+  for (let lh of horizontalJoin) {
+    if (lh.includes(word)) return true;
+  }
+  for (let lv of verticalJoin) {
+    if (lv.includes(word)) return true;
+  }
+  return false;
+};
+const transpose = function(matrix) {
+  let arr = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    arr.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      arr[i][j] = matrix[j][i];
     }
-}
+  }
+  return arr;
+};
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
